27eb41d9dccb1455bd39badd96f531cc40922dee
mishu-cse <64586256+mishu-cse@users.noreply.github.com>
2022-02-12 13:25:29 -0800

    Add files via upload

---
+++ HelloSpringMVC/src/main/java/net/codejava/springmvc/App.java
@@ -0,0 +1,15 @@
+package net.codejava.springmvc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+public class App {
+	public static void main(String[] args) 
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"Spring-Module.xml");
+ 
+		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+		obj.printHello();
+	}
+}
